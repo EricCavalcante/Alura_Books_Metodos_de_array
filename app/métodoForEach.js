38ca8b22d2397a20edff37b4f7fe1945a43b0127
Eric Cavalcante <g4mesh.bigorna@gmail.com>
2023-09-30 17:25:12 -0300

    criando os livros indisponiveis

--- app/métodoForEach.js
+++ app/métodoForEach.js
@@ -1,21 +1,31 @@
 const elementosParaInserirLivros = document.querySelector("#livros")
 
 function exibirOsLivrosNaTela(listaDeLivros){
-    elementosParaInserirLivros.innerHTML= ''
-    listaDeLivros.forEach(livro => {
-        elementosParaInserirLivros.innerHTML += `
-        <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" />
-        <h2 class="livro__titulo">
-          ${livro.titulo}
-        </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
-        <div class="tags">
-          <span class="tag">${livro.categoria}</span>
-        </div>
+  elementosParaInserirLivros.innerHTML= ''
+  listaDeLivros.forEach(livro => {
+    // let disponibilidade = verificarDisponibilidadeDoLivro(livro)  
+    let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
+    elementosParaInserirLivros.innerHTML += `
+      <div class="livro">
+      <img class="${disponibilidade}" src="${livro.imagem}" />
+      <h2 class="livro__titulo">
+        ${livro.titulo}
+      </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
       </div>
+    </div>
 
-        `
-    } )
+      `
+  } )
 }
+
+// function verificarDisponibilidadeDoLivro(livro){
+//   if (livro.quantidade > 0){
+//     return 'livro__imagens'
+//   }else{
+//     return 'livro__imagens indisponivel'
+//   }
+// }
